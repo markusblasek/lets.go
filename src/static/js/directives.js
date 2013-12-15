c07bea46bdf28c15ccbe39af6d202d78928070e8
Oliver Mader <b52@reaktor42.de>
2013-12-15 20:41:40 +0100

    Update chat directive

--- src/static/js/directives.js
+++ src/static/js/directives.js
@@ -11,22 +11,34 @@ angular.module('letsGo.directives', []).
       },
       templateUrl: '/static/partials/directives/message-box.jade',
       link: function(scope, element, attrs) {
-        var messages = element.find('.messages');
-        var input = element.find('input.text');
-        var send = element.find('.send');
+        var messages = element.find('.comments');
 
         scope.$on('message', function(event, message) {
           if (message.target.type === attrs.lgTargetType &&  message.target.id === attrs.lgTargetId) {
-            messages.append('<p><b>' + message.user + ': </b> ' + message.text + '</p>');
+            var html = '<div class="comment">' +
+                         '<a class="avatar">' +
+                           '<img src="http://robohash.org/' + message.user + '?set=set3&size=32x32">' +
+                         '</a>' +
+                         '<div class="content">' +
+                           '<a href="#/user/123" class="author">' + message.user + '</a>' +
+                           '<div class="text">' + message.text + '</div>' +
+                         '</div>' +
+                       '</div>';
+            messages.append(html);
+            messages.animate({scrollTop: messages.prop('scrollHeight')}, 500);
           }
         });
 
-        send.on('click', function() {
-          var text = input.val();
-          if (text && text != '') {
-            socket.message(attrs.lgTargetType, attrs.lgTargetId, text);
+        element.find('input.text').keyup(function(e) {
+          if (e.keyCode === 13) {
+            var $this = $(this);
+            var text = $this.val();
+            if (text && text != '') {
+              socket.message(attrs.lgTargetType, attrs.lgTargetId, text);
+              $this.val('');
+            }
           }
-        })
+        });
       }
     };
   });
